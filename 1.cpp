a42fd76ee6ed9bd82deb7bf655779a6b316ab65c
rahiov-yr <126478879+rahiov-yr@users.noreply.github.com>
2023-03-22 02:31:03 -0600

    Basic

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#define ll long long
+
+using namespace std;
+
+void solve(){
+  ll a, b, c;
+  cin >> a >> b >> c;
+  cout << a + b + c << endl;
+}
+
+int main(){
+ 
+  ll tt;
+  cin >> tt;
+  while(tt--)
+    solve();
+  
+  return 0;
+}
